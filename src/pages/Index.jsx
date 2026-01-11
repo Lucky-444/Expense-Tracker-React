929dc10a3d33e15cf2b2ae1573541017403c00bd
Lucky <sashankswain6@gmail.com>
2026-01-12 01:43:35 +0530

    feat: create DashboardLayout component with header, footer, and toast notifications

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { ExpenseProvider } from '../context/ExpenseContext'
+import DashboardLayout from '../layouts/DashboardLayout'
 
 const Index = () => {
-  return (
-    <div>Index</div>
-  )
+  return <ExpenseProvider>
+         <DashboardLayout>
+                  
+         </DashboardLayout>
+  </ExpenseProvider>
 }
 
 export default Index
